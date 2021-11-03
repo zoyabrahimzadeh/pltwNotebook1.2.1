ca0c8f831d5a8967cf253751208293fe947bb7f4
zoya b <88953192+zoyabrahimzadeh@users.noreply.github.com>
2021-11-03 16:29:14 +0000

    updated to step 33

--- main.py
+++ main.py
@@ -6,14 +6,29 @@ import random as rand
 turtle_color = 'lightgreen'
 turtle_size = 3
 turtle_shape = 'square'
+score = 0
 #-----initialize turtle-----
 shortie = turtle.Turtle()
+score_writer = turtle.Turtle()
 shortie.shape(turtle_shape)
 shortie.fillcolor(turtle_color)
 shortie.shapesize(turtle_size)
 #-----game functions--------
 def shortie_clicked(x, y):
-  print("shorties been clicked! got to move her!")
+  update_score()
+  change_position()
+def change_position():
+  new_xpos = rand.randint(-400,400)
+  new_ypos = rand.randint(-300, 300)
+  shortie.penup()
+  shortie.hideturtle()
+  shortie.goto(new_xpos, new_ypos)
+  shortie.showturtle()
+  shortie.pendown()
+def update_score():
+  global score
+  score += 1
+  print(score)
 
 #-----events----------------
 shortie.onclick(shortie_clicked)
